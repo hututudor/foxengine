de5f581914ea931d562695fd16d8d5672403bc7a
Hutu Tudor <hutu.tudor@yahoo.com>
2020-07-21 15:24:28 +0300

    Added json

--- src/ecs/color.h
+++ src/ecs/color.h
@@ -1,7 +1,11 @@
 #pragma once
+
 #include "../base.h"
 #include "component.h"
 
-struct Color: Component {
+struct Color : Component {
   glm::vec4 color;
+
+  Color() : color(glm::vec4(0, 0, 0, 0)) {
+  }
 };
--- src/runtime/main.cpp
+++ src/runtime/main.cpp
@@ -10,22 +10,6 @@ i32 main() {
 
   initRenderer();
 
-  entt::entity orangeObj= world.create();
-  auto& orangeTransform = world.emplace<Transform>(orangeObj);
-  orangeTransform.position = glm::vec2(100.0, 100.0);
-  orangeTransform.scale = glm::vec2(100.0, 50.0);
-  orangeTransform.rotation = 45;
-  auto& orangeColor = world.emplace<Color>(orangeObj);
-  orangeColor.color = glm::vec4(1.0f, 0.5f, 0.0f, 1.0f);
-
-//  entt::entity greenObj = world.create();
-//  auto& greenTransform = world.emplace<Transform>(greenObj);
-//  greenTransform.position = glm::vec2(200.0, 500.0);
-//  greenTransform.scale = glm::vec2(100.0, 50.0);
-//  greenTransform.rotation = 15;
-//  auto& greenColor = world.emplace<Color>(greenObj);
-//  greenColor.color = glm::vec4(1.0f, 0.5f, 0.0f, 1.0f);
-
   while (isWindowRunning()) {
     updateRenderer();
 
